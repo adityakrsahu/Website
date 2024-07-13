58d6ad66806e8852f438933d274fe2bc355cded5
aditya kumar <adityakr2019@gmail.com>
2024-07-14 00:41:41 +0530

    resume2

--- frontend/project/src/pages/resume.jsx
+++ frontend/project/src/pages/resume.jsx
@@ -3,7 +3,34 @@ import React from 'react'
 function Resume() {
   return (
     <>
-    
+    <div className='container'>
+    <h1>WHO AM I ?</h1>
+
+
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-body">
+              <h2  class="card-title resume">Student</h2>
+              <h4 class="card-text">I Joined haryana, passed out Diploma (CSE) from HSBTE,
+               Where I found my way towards IT field. Currently pursuing in  
+               Python full stack devloper in IT field.</h4>
+              
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="card resume ">
+            <div class="card-body">
+              <h2 class="card-title resume">Programmer</h2>
+              <h4 class="card-text">Currently into Web Development🕸️and learning Programming Languages like Html, Css, Js, React,  Python Django .. etc with ✨. Created different project related to it.</h4>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
     </>
   )
 }
