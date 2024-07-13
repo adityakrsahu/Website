4ff10336796ee21e76e2e3bed37da131de5d9a81
aditya kumar <adityakr2019@gmail.com>
2024-07-14 00:14:20 +0530

    about

--- frontend/project/src/pages/about.jsx
+++ frontend/project/src/pages/about.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function About() {
   return (
-    <>
+    <><div className='about'>
         <div className='container'>
             <div>
                 <div></div>
@@ -12,9 +12,13 @@ function About() {
                     <h4>Student and a Developer</h4>
                     <h4>with Different Programming lang</h4>
                     <h4>Connect over:  <Link to="https://www.linkedin.com/in/aditya-kumar-068169212/">LinkedIn👈</Link>  </h4>
+                    <h4> Visit:   <Link to="https://github.com/adityakrsahu">GitHub👈</Link>  </h4>
+                    <h4>Follow on: <Link to="https://www.linkedin.com/in/aditya-kumar-068169212/"> Instagram👈(@adityasahu_023)</Link>  </h4>
+ 
                 </div>
             </div>
         </div>
+      </div>
     </>
   )
 }
